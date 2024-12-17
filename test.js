379f571e0af1a75787bf365e7dd99809fee392da
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-12-17 10:42:40 +0100

    changes

--- test.js
+++ test.js
@@ -13,7 +13,21 @@ class Person{
     }
 }
 
+class Employee extends Person{
+    constructor(name, age, job, income, position){
+        super(name, age);
+        this.job = job;
+        this.income = income;
+        this.position = position;
+    }
+
+    EmployeeSayHi(){
+        console.log(`Hi, my name is ${this.name} and I am ${this.age} years old and I am a ${this.job} and I earn ${this.income} and I am an ${this.position}`);
+    }
+}
+
 let PersonOne = new Person('Morten', 28);
-console.log(PersonOne);
+let EmployeeOne = new Employee('Morten', 28, 'Developer', 50000, 'Junior');
 
 PersonOne.SayHi();
+EmployeeOne.EmployeeSayHi();
