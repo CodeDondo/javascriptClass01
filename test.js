62d96b312cb2d186c40a4803b1b8cea147a34bf2
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-12-17 09:33:14 +0100

    changes

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 class Person{
 
+    #PrivateAbility;
+
     constructor (name, age){
         this.name = name;
         this.age = age;
+        this.#PrivateAbility = 'I am a private ability';
+    }
+
+    SayHi(){
+        console.log(`Hi, my name is ${this.name} and I am ${this.age} years old and this is my private ability: ${this.#PrivateAbility}`);
     }
 }
 
 let PersonOne = new Person('Morten', 28);
 console.log(PersonOne);
+
+PersonOne.SayHi();
